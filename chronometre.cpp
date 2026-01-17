5db764b02f5ab9e842ec38ec24066b67cd0c9150
Dirk Eddelbuettel <edd@debian.org>
2026-01-17 16:19:38 -0600

    Added comments to chronometre.cpp

--- chronometre.cpp
+++ chronometre.cpp
@@ -2,7 +2,7 @@
 #include <pybind11/chrono.h>
 
 // Inspired by an existing class 'stopwatch' in spdlog
-// but removing ftm header and compacted for example here
+// but removing ftm header and compacted for the example use here
 
 // Copyright(c) 2015-present, Gabi Melman & spdlog contributors.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
@@ -30,21 +30,36 @@
 // "Elapsed 5ms"
 
 class stopwatch {
+    // internal state variable of clock time point 'when started'
     std::chrono::time_point<std::chrono::steady_clock> start_tp_;
 public:
+    // default constructor initialising stopwatch as of 'right now'
     stopwatch() : start_tp_{std::chrono::steady_clock::now()} {}
+
+    // Added constructor from string which is used to receive the _address_ of another
+    // instance from which the value of the one member variable 'start_tp_' is copied
+    // This is usable from R in the context of this example, but is not advised from
+    // Python due to object lifetime considerations; we may need a static factory function
     stopwatch(std::string& s) {
         auto sp = static_cast<stopwatch*>( (void*) strtol(s.c_str(), nullptr, 0) );
         start_tp_ = sp->start_tp_;
     }
+
+    // standard accessor to determine elapsed time in seconds as duration object
+    // add .count() to convert to to double instead of duration object (which has formating)
     std::chrono::duration<double> elapsed() const {
         return std::chrono::duration<double>(std::chrono::steady_clock::now() - start_tp_);
     }
+
+    // helper to reset internal clock to current time
     void reset() {
         start_tp_ = std::chrono::steady_clock::now();
     }
 };
 
+// Standard pybind11 wrapping of C++ class 'stopwatch' into a module 'Stopwatch'
+// exported by this 'chronometre' Python package. The second constructor can be
+// used to pass a pointer to an existing instance (as a string containing the address)
 PYBIND11_MODULE(chronometre, m) {
     pybind11::class_<stopwatch>(m, "Stopwatch")
         .def(pybind11::init<>())

19e597a10660f2e07efd6839839683a3f4400b3c
Dirk Eddelbuettel <edd@debian.org>
2026-01-19 11:06:14 -0600

    Expand documentation in README.md and main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
 import chronometre
+import time
 
 def main():
     s = chronometre.Stopwatch()
     print(s)
     print(s.elapsed())
+    time.sleep(0.1)
+    print(s.elapsed())
 
     s2 = chronometre.bake(s)
     print(s2)
     print(s2.elapsed())
+    time.sleep(0.1)
+    print(s2.elapsed())
+
+    print(s.elapsed())
 
 if __name__ == "__main__":
     main()
